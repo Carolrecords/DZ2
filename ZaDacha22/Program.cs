fe40ad0c3c970f2c32f5ec60e4282a6a8e552e60
korolev_as <antonykas@mail.ru>
2022-11-18 02:35:08 +0300

    init commit

---
+++ ZaDacha22/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третье число -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьего числа нет");
+}
+
---
+++ ZaDacha23/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите пожалуйста цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) 
+{
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("(этот день недели является выходным) -> да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("это день недели, вообще не день недели)");
+  }
+  else Console.WriteLine("(этот день недели не выходной) -> нет");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
+
